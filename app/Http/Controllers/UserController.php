f39255155f883ba03b865d30d7a89e44999baf21
Vass Gyöngyi Mária (SZF_E_2022_2024) <vass.gyongyi.maria@diak.szamalk-szalezi.hu>
2023-12-06 20:16:32 +0100

    könyvtár db-órai munka

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = response()->json(User::all());
+        return $users;
+    }
+
+    public function show($id)
+    {
+        $user = response()->json(User::find($id));
+        return $user;
+    }
+
+    public function store(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->permission = $request->permission;
+        $user->save();
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->permission = $request->permission;
+        $user->save();
+    }
+    public function destroy($id)
+    {
+        //find helyett a paraméter
+        User::find($id)->delete();
+    }
+
+
+    public function updatePassword(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            "password" => 'string|min:3|max:50'
+        ]);
+        if ($validator->fails()) {
+            return response()->json(["message" => $validator->errors()->all()], 400);
+        }
+        $user = User::where("id", $id)->update([
+            "password" => Hash::make($request->password),
+        ]);
+        return response()->json(["user" => $user]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CopyController;
+use App\Http\Controllers\LendingController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::apiResource('/users',UserController::class);
+Route::apiResource('/copies',CopyController::class);
+Route::apiResource('/books',BookController::class);
+Route::get('/lendings', [LendingController::class, 'index']);
+Route::post('/lendings', [LendingController::class, 'store']);
+Route::get('/lendings/{user_id}/{copy_id}/{start}', [LendingController::class, 'show']);
+Route::put('/lendings/{user_id}/{copy_id}/{start}', [LendingController::class, 'update']);
+Route::delete('/lendings/{user_id}/{copy_id}/{start}', [LendingController::class, 'destroy']);
